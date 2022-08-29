32d7e939ae3dedf43e09207f4afd1823d94c101b
LiquidJaguar10 <83448848+LiquidJaguar-10@users.noreply.github.com>
2022-08-29 23:02:25 +0530

    Add files via upload

---
+++ rock paper scissors.py
@@ -0,0 +1,55 @@
+import random
+play_again = 'y'
+
+a = input('Enter your name whoever you are:')
+print('Welcome to my arena:', a)
+
+inputs = ["rock", "paper","scissors"]
+bot = random.choice(inputs)
+player = None
+
+while player not in inputs:
+    player = input('rock, paper or scissors?').lower()
+    if player not in inputs:
+        print('Enter a valid choice')
+
+if player == bot:
+    print('Player\'s choice:', player)
+    print('Bot\'s choice:', bot)
+    print('Scores are level!')
+elif player == 'paper':
+    if bot == 'scissors':
+        print('Player\'s choice:',player)
+        print('Bot\'s choice:',bot)
+        print('You lost haha get good!')
+    if bot == 'rock':
+        print('Player\'s choice:',player)
+        print('Bot\'s choice:', bot)
+        print('You Won the game!!! Now say SIUUUU')
+elif player == 'rock':
+    if bot == 'scissors':
+        print('Player\'s choice:',player)
+        print('Bot\'s choice:',bot)
+        print('You Won the game!!! Now say SIUUUU')
+    if bot == 'paper':
+        print('Player\'s choice:',player)
+        print('Bot\'s choice:', bot)
+        print('You lost haha get good!')
+elif player == 'scissors':
+    if bot == 'rock':
+        print('Player\'s choice:',player)
+        print('Bot\'s choice:',bot)
+        print('You lost haha get good!')
+    if bot == 'paper':
+        print('Player\'s choice:',player)
+        print('Bot\'s choice:', bot)
+        print('You Won the game!!! Now say SIUUUU')
+
+    play_again = input('Do you fancy playing another game? (y/n)')
+    if play_again == 'y':
+        break
+
+print('Bye')
+
+
+ 
